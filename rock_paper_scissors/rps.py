ea2ba6cf48929b02e07a8d675f208136730a7898
Alec <afroenygam@gmail.com>
2018-09-04 16:32:20 -0400

    rock_paper_scissors updated (Not Working)

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,21 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    allPossibilities = []
+    options = ['rock', 'paper', 'scissors']
+    def build_permutations(round, roundNumber):
+        for i in range(0, len(options)):
+            round.append(options[i])
+            print('roudn', round)
+            if n == roundNumber:
+                return allPossibilities.append(round)
+            else:
+                build_permutations(round, roundNumber + 1)
+            round.pop(len(round)-1)
+    build_permutations([], 1)
+    return allPossibilities
+
+
 
 if __name__ == "__main__":
   # Test out your implementation from the command line
